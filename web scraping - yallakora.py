a5561c804b44813a54a68a3f979ee391db9b99ff
ossamaxg <122169389+ossamaxg@users.noreply.github.com>
2023-03-08 14:54:24 +0200

    Add files via upload

---
+++ web scraping - yallakora.py
@@ -0,0 +1,45 @@
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+
+start = time.time()
+date = input('Would you enter date please (like 21/mar/23 or 21mar23): ')
+page = requests.get(f'https://www.yallakora.com/match-center/مركز-المباريات?date={date}#days')
+def main(page):
+    src = page.content
+    soup = BeautifulSoup(src, 'lxml')
+    matches_details = []
+    champs = soup.find_all('div', {'class': 'matchesList'})
+    for champ in champs:
+        champ_url = 'https://www.yallakora.com' + champ.a['href']
+        champ_title = champ.h2.text.strip()
+        champ_date = champ.find('div', {'class': 'date'}).text.strip()
+        matches = champ.find_all('li', {'class': 'item finish'})
+        def get_matches_details(matches):
+            for match in matches:
+                match_url = 'https://www.yallakora.com' + match.a['href']
+                match_status = match.span.text.strip()
+                team_A = match.p.text.strip()
+                team_B = match.find('div', {'class': 'teams teamB'}).p.text.strip()
+                score_1 = match.find('span', {'class': 'score'}).text.strip()
+                score_2 = match.find_all('span', {'class': 'score'})[1].text.strip()
+                match_result = f'{score_2} - {score_1}'
+                teams = f'{team_A} - {team_B}'
+                match_time = match.find('span', {'class': 'time'}).text.strip()
+                try:
+                    channel = match.find('div', {'class': 'channel icon-channel'}).text.strip()
+                except:
+                    channel = '???'
+                matches_details.append({'رابط البطولة': champ_url, 'عنوان البطولة': champ_title, 'مرحلة البطولة': champ_date, 'رابط المباراة': match_url, 'حالة المباراة': match_status, 'الفرق': teams, 'النتيجة': match_result, 'وقت المباراة': match_time, 'القناة': channel})
+        get_matches_details(matches)
+        keys = matches_details[0].keys()
+        with open(f'yallakora/matches_details_{date.replace("/", "")}.csv', 'w') as op_file:
+            dict_writer = csv.DictWriter(op_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(matches_details)
+    print('\n done \n', time.time() - start, 'seconds')
+main(page)
+
+
